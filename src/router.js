50469a56484f3c9663df7e33a922e8a86218cda7
Ashish Vagish <ashishvagish@outlook.in>
2019-09-19 19:39:30 +0530

    lazy loading view components

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import Register from './views/Register.vue'
 import store from './store';
 
 Vue.use(Router)
@@ -16,17 +14,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
       path: '/login',
       name: 'login',
-      component: Login,
+      component: () => import( /* webpackChunkName: "login" */ './views/Login.vue'),
       beforeEnter: (to, from, next) => {
 
         if (store.getters.isLoggedIn)
@@ -38,7 +28,7 @@ export default new Router({
     {
       path: '/register',
       name: 'register',
-      component: Register,
+      component: () => import( /* webpackChunkName: "register" */ './views/Register.vue'),
       beforeEnter: (to, from, next) => {
 
         if (store.getters.isLoggedIn)
